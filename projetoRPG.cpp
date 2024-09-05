a52008371e5fa333d22df21a8543e39509ea36bb
Kaio <kaiogabrielrozini@gmail.com>
2024-09-04 23:57:46 -0300

    versão 1.0

--- projetoRPG.cpp
+++ projetoRPG.cpp
@@ -49,7 +49,12 @@ class Protagonist {
             level += 1;
             maxHealth += 30;
             health += 30;
-            xp = 0;
+            if(xp == 150){
+                xp = 50;
+            } else {
+                xp = 0;
+            }
+            
             cout << "Voce subiu de nivel!\n";
         }
         
@@ -194,6 +199,8 @@ void combatStats(Protagonist &protag, Piece &piec){
         cout << "\x1B[2J\x1B[H";
     } else {
         cout << "Voce foi de dormes :(";
+        sleep(2);
+        cout << "\x1B[2J\x1B[H";
     }
     protag.running = false;
     
@@ -275,7 +282,7 @@ int main(){
 
     srand(time(0));
 
-      string sla  =  "   _____ _                     ______                 _            \n"
+      string title  ="   _____ _                     ______                 _            \n"
                      "  / ____| |                   |  ____|               (_)           \n"
                      " | |    | |__   ___  ___ ___  | |__   _ __ ___  _ __  _ _ __ ___   \n"
                      " | |    | '_ | / _ |/ __/ __| |  __| | '_ ` _ || '_ || | '__/ _ |  \n"
@@ -285,25 +292,25 @@ int main(){
                      "                                             |_|                   \n";
 
     cout << "Bem vindo ao meu RPG de Texto!";
-    sleep(1);
+    sleep(3);
     cout << "\x1B[2J\x1B[H";
 
-    cout << sla << endl;   
-    sleep(3);
+    cout << title << endl;   
+    sleep(5);
     cout << "\x1B[2J\x1B[H";                                     
     
     //Contando historia e criando personagem
     Protagonist protagonist;
     string name;
-    cout << "Uma batalha foi travada, Brancas VS Pretas, foi uma guerra devastadora que terminou em 77 movimentos, todas as pecas pretas foram mortas exceto por voce, o rei preto\n Agora em um mundo governado pelo cruel Rei das Pecas Brancas, onde a liberdade foi suprimida e a justica e apenas uma lembranca.\nDeterminado a minar o poder do Imperio e restaurar a liberdade ao continente de Xadrez.\nSua missao e clara: desafiar a tirania e um dia, enfrentar o proprio Rei Branco.\nO destino do Xadrez depende de suas acoes." << endl;
-    //sleep(3);
+    cout << "Uma batalha foi travada, Brancas VS Pretas, foi uma guerra devastadora que terminou em 77 movimentos, todas as pecas pretas foram mortas exceto por voce, o rei preto\nAgora em um mundo governado pelo cruel Rei das Pecas Brancas, onde a liberdade foi suprimida e a justica e apenas uma lembranca.\nDeterminado a minar o poder do Imperio e restaurar a liberdade ao continente de Xadrez.\nSua missao e clara: desafiar a tirania e um dia, enfrentar o proprio Rei Branco.\nO destino do Xadrez depende de suas acoes." << endl;
+    sleep(3);
     cout << "Qual o seu nome ?" << endl;
     cin >> name;
     protagonist.setProtagonist(name);
-    //sleep(1);
+    sleep(1);
     cout << "\x1B[2J\x1B[H";
     cout << "Nome: " << protagonist.name << endl << "Nivel: " << protagonist.level << endl << "Vida: " << protagonist.health << endl;
-    //sleep(2);
+    sleep(2);
     cout << "\x1B[2J\x1B[H";
     
     
@@ -346,82 +353,109 @@ int main(){
     Piece allPieces[14] = {paw1, paw2, paw3, paw4, paw5, paw6, paw7, paw8, bishop1, bishop2, knight1, knight2, rook1, rook2};
     
     
-    bool final;
-    do{
-        final = true;
-        int j;
-        do{
-            j  = rand() % 14;
-        } while(allPieces[j].health <= 0);
+    // //sistema de historia e combate
+    // bool final;
+    // do{
+    //     final = true;
+    //     int j;
+    //     do{
+    //         j  = rand() % 14;
+    //     } while(allPieces[j].health <= 0);
         
-        moving(protagonist, allPieces[j]);
-
-        if(allPieces[0].health <= 0 && allPieces[1].health <= 0 && allPieces[2].health <= 0 && allPieces[3].health <= 0 
-        && allPieces[4].health <= 0 && allPieces[5].health <= 0 && allPieces[6].health <= 0 && allPieces[7].health <= 0 
-        && allPieces[8].health <= 0 && allPieces[9].health <= 0 && allPieces[10].health <= 0 && allPieces[11].health <= 0
-        && allPieces[12].health <= 0 && allPieces[13].health <= 0){
-            final = false;
-        }
-    } while(final);
+    //     if(protagonist.health <= 0){
+    //         final = false;
+    //     }
+    //     moving(protagonist, allPieces[j]);
+    //     if(protagonist.health <= 0){
+    //         final = false;
+    //     }
+    //     if(allPieces[0].health <= 0 && allPieces[1].health <= 0 && allPieces[2].health <= 0 && allPieces[3].health <= 0 
+    //     && allPieces[4].health <= 0 && allPieces[5].health <= 0 && allPieces[6].health <= 0 && allPieces[7].health <= 0 
+    //     && allPieces[8].health <= 0 && allPieces[9].health <= 0 && allPieces[10].health <= 0 && allPieces[11].health <= 0
+    //     && allPieces[12].health <= 0 && allPieces[13].health <= 0){
+    //         final = false;
+    //     }
+    // } while(final);
 
     
-
-    cout << "Parabens! voce conseguiu derrotar todas as pecas brancas!\n";
-    sleep(1);
-    cout << "Agora so resta subir o castelo onde o rei esta\n";
-    sleep(1);
-    cout << "voce sobre toda a escadaria e la esta ele, O Rei Branco\n";
-    sleep(1);
-    cout << "Mas espere, algo esta errado, voce sente um calafrio subindo pela sua espinha, seus batimentos aceleram, voce sente como se estivesse prestes a morrer e entao...\n";
-    sleep(1);
-    cout << "Das sombras surje...\n";
-    sleep(1);
-    cout << "A Rainha Branca!";
-    sleep(3);
-    cout << "\x1B[2J\x1B[H";
-
-
-    combatStats(protagonist, queen);
-
-    if(queen.health > 0){
-        cout << "A rainha era muito forte, voce se retira, isolado e sem dignidade, voce jura voltar um dia e terminar oque comecou";
+    if(protagonist.health <= 0) {
+        cout << "Voce lutou bravamente, mas o inimigo era muito forte, voce deixou esta terra buscando o seu sonho...";
     } else {
+        cout << "Parabens! voce conseguiu derrotar todas as pecas brancas!\n";
+        sleep(3);
+        cout << "Agora so resta subir o castelo onde o rei esta\n";
+        sleep(3);
+        cout << "voce sobe toda a escadaria e la esta ele, O Rei Branco\n";
+        sleep(3);
+        cout << "Mas espere, algo esta errado, voce sente um calafrio subindo pela sua espinha, seus batimentos aceleram, voce sente como se estivesse prestes a morrer e entao...\n";
+        sleep(3);
+        cout << "Das sombras surje...\n";
+        sleep(3);
+        cout << "A Rainha Branca!";
+        sleep(5);
         cout << "\x1B[2J\x1B[H";
 
-        dialog(protagonist, king, "Eu: (Entrando na sala do trono, espadas em maos) Finalmente, estamos cara a cara, depois de todo o sangue que derramei para chegar ate aqui.");
-        dialog(protagonist, king, "Rei Branco: (Com um olhar calmo, mas cansado) Voce veio para vingar o que perdi, nao e? Seu trono, sua gloria... sua honra. Mas me diga, valeu a pena o caminho de destruicao ate aqui?");
-        dialog(protagonist, king, "Eu: Cada passo, cada batalha. Eu vim terminar o que voce comecou quando tomou meu reino e massacrou meu povo.");
-        dialog(protagonist, king, "Rei Branco: (Se levantando do trono, sem empunhar armas) E agora voce esta diante de sua vinganca. Mas antes de empunhar sua espada contra mim, considere o que realmente deseja. Morte? Justica? Ou apenas o fim deste ciclo de odio?");
-        dialog(protagonist, king, "Eu: Não posso confiar em um ditador como voce");
-        dialog(protagonist, king, "Rei Branco: Talvez. Mas pense no que vem depois. Mate-me, e voce se tornara o monstro que jurei derrotar. Poupe-me, e talvez possamos encontrar um caminho melhor... ou faca algo inesperado e una forcas contra um inimigo maior que ambos.");
 
-        int choose;
-        cout << "1. Xeque-mate: Matar o rei branco\n"
-                "2. En passant: Exilar o rei branco\n"
-                "3. Draw: Perdoar o rei branco\n";
+        //combatStats(protagonist, queen);
+        queen.health = -100;
 
-        cin >> choose;
-
-        if(choose == 1){
-            cout << "Eu: Não há caminho melhor para nós. Hoje, você paga por tudo.\n\n";      
-            cout << "Rei Branco: (Sorriso amargo) Então este é o fim... Que minha morte pese sobre sua consciência para sempre.\n\n";
-            cout << "Eu: (Atinge o rei inimigo com sua espada) Adeus, velho inimigo. Finalmente, meu reino está vingado.\n\n";
-            cout << "(O sangue do rei inimigo mancha o chão. O silêncio ecoa no salão do trono. O novo rei assume, mas em seu coração, a paz nunca virá. O ciclo de ódio continua, agora com ele no comando.)";
-              
-        } else if (choose == 2){
-            cout << "Eu: Não. Não matarei você. O ciclo de vingança termina aqui. Eu o exilo deste reino para nunca mais retornar.\n\n";
-            cout << "Rei Branco: (Olhos arregalados, incrédulo) Você... me poupa? Por quê?\n\n";
-            cout << "Eu: Porque não quero ser como você. Vou governar com justiça, algo que você jamais fez.\n\n";
-            cout << "Rei Branco: (Abaixando a cabeça) Talvez... talvez haja redenção para mim após tudo isso. Obrigado... por esta segunda chance.\n\n";
-            cout << "(O rei inimigo é exilado, e o novo governante é celebrado por sua sabedoria e misericórdia. Uma nova era de paz começa, guiada pela escolha inesperada do perdão.)";
+        //Finais
+        if(queen.health > 0){
+            cout << "A rainha era muito forte, voce se retira, isolado e sem dignidade, voce jura voltar um dia e terminar oque comecou";
         } else {
-            cout << "Eu: Talvez tenha razão. A morte não é o caminho. Vamos pôr nossas diferenças de lado. Uma ameaça maior se aproxima, e nossos reinos precisam de paz.\n\n";
-            cout << "Rei Branco: (Surpreso) Você sugere... uma aliança? Após tudo o que fiz?\n\n";
-            cout << "Eu: Sim. Mas saiba que a confiança deve ser construída. Lute ao meu lado e prove seu valor.\n\n";
-            cout << "Rei Branco: (Com um aceno firme) Então que assim seja. Por nossos povos, lutaremos juntos.\n\n";
-            cout << "Unidos por uma causa comum, os dois antigos inimigos formam uma aliança inesperada. Juntos estabelecem um novo capítulo de cooperação entre seus reinos.";
+            cout << "\x1B[2J\x1B[H";
+
+            dialog(protagonist, king, "Eu: (Entrando na sala do trono, espadas em maos) Finalmente, estamos cara a cara, depois de todo o sangue que derramei para chegar ate aqui.");
+            dialog(protagonist, king, "Rei Branco: (Com um olhar calmo, mas cansado) Voce veio para vingar o que perdi, nao e? Seu trono, sua gloria... sua honra. Mas me diga, valeu a pena o caminho de destruicao ate aqui?");
+            dialog(protagonist, king, "Eu: Cada passo, cada batalha. Eu vim terminar o que voce comecou quando tomou meu reino e massacrou meu povo.");
+            dialog(protagonist, king, "Rei Branco: (Se levantando do trono, sem empunhar armas) E agora voce esta diante de sua vinganca. Mas antes de empunhar sua espada contra mim, considere o que realmente deseja. Morte? Justica? Ou apenas o fim deste ciclo de odio?");
+            dialog(protagonist, king, "Eu: Não posso confiar em um ditador como voce");
+            dialog(protagonist, king, "Rei Branco: Talvez. Mas pense no que vem depois. Mate-me, e voce se tornara o monstro que jurei derrotar. Poupe-me, e talvez possamos encontrar um caminho melhor... ou faca algo inesperado e una forcas contra um inimigo maior que ambos.");
+
+            int choose;
+            cout << "1. Xeque-mate: Matar o rei branco\n"
+                    "2. En passant: Exilar o rei branco\n"
+                    "3. Draw: Perdoar o rei branco\n";
+
+            cin >> choose;
+
+            if(choose == 1){
+                cout << "Eu: Não há caminho melhor para nós. Hoje, você paga por tudo.\n\n";
+                sleep(5);
+                cout << "Rei Branco: (Sorriso amargo) Então este é o fim... Que minha morte pese sobre sua consciência para sempre.\n\n";
+                sleep(5);
+                cout << "Eu: (Atinge o rei inimigo com sua espada) Adeus, velho inimigo. Finalmente, meu reino está vingado.\n\n";
+                sleep(5);
+                cout << "(O sangue do rei inimigo mancha o chão. O silêncio ecoa no salão do trono. O novo rei assume, mas em seu coração, a paz nunca virá. O ciclo de ódio continua, agora com ele no comando.)";
+                
+            } else if (choose == 2){
+                cout << "Eu: Não. Não matarei você. O ciclo de vingança termina aqui. Eu o exilo deste reino para nunca mais retornar.\n\n";
+                sleep(5);
+                cout << "Rei Branco: (Olhos arregalados, incrédulo) Você... me poupa? Por quê?\n\n";
+                sleep(5);
+                cout << "Eu: Porque não quero ser como você. Vou governar com justiça, algo que você jamais fez.\n\n";
+                sleep(5);
+                cout << "Rei Branco: (Abaixando a cabeça) Talvez... talvez haja redenção para mim após tudo isso. Obrigado... por esta segunda chance.\n\n";
+                sleep(5);
+                cout << "(O rei inimigo é exilado, e o novo governante é celebrado por sua sabedoria e misericórdia. Uma nova era de paz começa, guiada pela escolha inesperada do perdão.)";
+            } else {
+                cout << "Eu: Talvez tenha razão. A morte não é o caminho. Vamos pôr nossas diferenças de lado. Uma ameaça maior se aproxima, e nossos reinos precisam de paz.\n\n";
+                sleep(5);
+                cout << "Rei Branco: (Surpreso) Você sugere... uma aliança? Após tudo o que fiz?\n\n";
+                sleep(5);
+                cout << "Eu: Sim. Mas saiba que a confiança deve ser construída. Lute ao meu lado e prove seu valor.\n\n";
+                sleep(5);
+                cout << "Rei Branco: (Com um aceno firme) Então que assim seja. Por nossos povos, lutaremos juntos.\n\n";
+                sleep(5);
+                cout << "Unidos por uma causa comum, os dois antigos inimigos formam uma aliança inesperada. Juntos estabelecem um novo capítulo de cooperação entre seus reinos.";
+            }
         }
+
     }
 
-    
+    sleep(20);
+    cout << "\x1B[2J\x1B[H";
+
+    cout << "Muito obrigado por jogar!\n\n";
+    cout << title;
 }
